c52449f9e14b427507cec845bbff85dbecdf3e5a
Elelia <llucas@ensup.eu>
2025-06-24 12:13:07 +0200

    add nginx and docker

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const app = require('./src/app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const db = require('./src/config/dbConfig');
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+  try {
+    // Test de connexion à la base via le pool exporté
+    const connection = await db.getConnection();
+    console.log("Connected to MySQL database!");
+    connection.release();
+
+    // Si la connexion fonctionne, on lance le serveur
+    app.listen(PORT, () => {
+      console.log(`Auth service running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MySQL:", err);
+    process.exit(1); // empêche le lancement de l'app si la DB échoue
+  }
+})();
 
 //utiliser un revserse proxy pour le déploiement
---
+++ src/config/dbConfig.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD
+});
+
+module.exports = pool;
