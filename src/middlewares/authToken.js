e3af050522a48ea9957c1886377fc0f4a18d3fdf
Elelia <l.lucas@lilo.org>
2025-08-15 16:22:49 +0200

    fix access token

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.generateAccessToken = (user) => {
   return jwt.sign(
-    { id: user.id, email: user.email },
+    { id: user.id, role: user.role },
     process.env.JWT_SECRET,
     { expiresIn: '15m' }
   );
