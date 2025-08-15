fcd91ae5db16d5e03a9bbf28a92abaedba5d3d5d
Elelia <l.lucas@lilo.org>
2025-08-15 10:36:04 +0200

    add role in token

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -10,7 +10,7 @@ exports.generateAccessToken = (user) => {
 
 exports.generateRefreshToken = (user) => {
   return jwt.sign(
-    { id: user.id, email: user.email },
+    { id: user.id, role: user.role },
     process.env.JWT_REFRESH_SECRET,
     { expiresIn: '7d' }
   );
